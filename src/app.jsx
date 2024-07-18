0a00f6da7a14a4c06ae140a060927d423cc9795a
camilomaxius05 <camilobm05@gmail.com>
2024-07-18 17:27:55 -0500

    first and only commit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import TwitterFollowCard from './TwitterFollowCard'
+import './app.css'
+
+export default function App(){
+    return(
+        <section className='App'>
+            <TwitterFollowCard  userName="HptasemedañoelPC" name="Alejandro" />
+            <TwitterFollowCard  userName="Bobesponja" name="BobToronja" />
+        </section>
+        
+    )
+}
